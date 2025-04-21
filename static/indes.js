54a503524afdd7fd95ff50bb1821164d57c8fd1c
farbod <farbudskate@gmail.com>
2025-04-21 20:31:34 +0430

    seconde commit

--- static/indes.js
+++ static/indes.js
@@ -1,10 +1,34 @@
-const todoList = [];
+const todoList = ['kjhg', 'jhgf'];
 
-function addTodo(){
-const inputElement =  document.querySelector(".jsin");
-const name = inputElement.value;
+renderTodoList();
 
-todoList.push(name);
-console.log(todoList);
+function renderTodoList() {
+
+    let todoListHTML = '';
+
+    for (i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</P>`;
+        todoListHTML += html;
+    }
+
+    console.log(todoListHTML)
+    document.querySelector('.jstd')
+        .innerHTML = todoListHTML;
 
 }
+
+
+function addTodo() {
+    const inputElement = document.querySelector(".jsin");
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+
+    inputElement.value = '';
+    renderTodoList();
+}
+
+
